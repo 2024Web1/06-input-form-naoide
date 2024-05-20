35a473cc12615ed84ac443f77b1875dc9f00eb9d
idenao <kd1350189@st.kobedenshi.ac.jp>
2024-05-20 13:29:11 +0900

    passward

---
+++ public/passward.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charaset="utf-8">
+<title>passward</title>
+</head>
+<body>
+<table border="1">
+    <tr>
+        <td>名前</td><td><?php echo $_POST["name"] ?></td>
+</tr>
+<tr>
+    <td>passward</td><td><?php echo $_POST["pass"] ?></td>
+</tr>
+</table>
+</body>
+</html
+
+    
